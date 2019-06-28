80dcdfe711e84897a83d5fa2d1440739c6aa0ce1
Radu Stefanescu <stefanescuradu@gmail.com>
2019-06-28 15:14:30 +0300

    Initial commit. Initialise legal-consultation project and dependencies.

---
+++ src/main/java/com/code4ro/legalconsultation/LegalConsultationApplication.java
@@ -0,0 +1,14 @@
+package com.code4ro.legalconsultation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalConsultationApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LegalConsultationApplication.class, args);
+	}
+
+}
